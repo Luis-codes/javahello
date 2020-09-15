bf008204fc3b30e3238ee30724dc31efa3b671e8
Luis rosado <luismrosado@yahoo.com>
2020-09-15 17:39:21 -0400

    java expressions

--- src/Hello.java
+++ src/Hello.java
@@ -3,8 +3,12 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, Master Luis");
 
-        int myFirstNumber = 5;
-
-        System.out.println(myFirstNumber);
+        int myFirstNumber = (7 + 10) + (3 * 6);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
     }
 }
