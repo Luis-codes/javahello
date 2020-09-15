92bfc984b77b38a9ecdd4e45417ac5b60c3606f3
Luis rosado <luismrosado@yahoo.com>
2020-09-15 13:01:50 -0400

    day 1

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello, Master Luis!");
+        System.out.println("Hello, Master Luis");
 
         int myFirstNumber = 5;
     }
